267ae2d732c6f20b4cc0f982ebd171d92e05bd8b
Marco Jasso <mjasso@itexico.net>
2017-02-17 23:52:37 -0600

    lastFM - parseQuery is good

--- mediaAPI/common/lastfm/lastFM.js
+++ mediaAPI/common/lastfm/lastFM.js
@@ -10,8 +10,23 @@ var buildResponse = function() {
   console.log(chalk.magenta('method: %s'), 'buildResponse');
 };//buildResponse
 
-var parseQuery = function() {
+var parseQuery = function(query, next) {
   console.log(chalk.magenta('method: %s'), 'parseQuery');
+  //Check whether a search term has been provided or not
+  //if not se`rch term was provided default to error
+  if (!query.term) {
+    var err = new Error();
+    err.name = 'Bad name';
+    err.status = 404;
+    err.message = 'Missing search term, Unable to perform search.';
+    return next(err);
+  }
+  // handle success
+  var newQuery = {};
+  newQuery.term  = query.term;
+  newQuery.items = !query.items ? 15 : query.items;
+  newQuery.opage = !query.page ? 1 : query.page;
+  next(null, newQuery);
 };//parseQuery
 
 var searchByTrack = function() {
--- mediaAPI/common/models/song.js
+++ mediaAPI/common/models/song.js
@@ -7,14 +7,22 @@ var lastFM  = require('../lastfm/lastFM');
 module.exports = function(Song) {
   Song.search = function(name, items, page, cb) {
     console.log(chalk.cyan('remote method: %s'), 'Song.search');
-    cb(null, 'Hello I am Song.search');
-  };
+    var query = {term: name, items: items, page: page};
+    lastFM.query(query, function(error, parsedQuery) {
+      //check for error
+      if (error) { return cb(error, null); }
+      //handle success
+      cb(null, 'query is good!');
+    });//query
+  };//Song.search
+
   Song.remoteMethod('search', {
+    http: {path: '/search', verb: 'get'},
     accepts: [
       {arg: 'name', type: 'string'},
       {arg: 'items', type: 'number'},
       {arg: 'page', type: 'number'},
     ],
-    returns: {type: 'array', root: true},
+    returns: {type: 'json', root: true},
   });
 };
