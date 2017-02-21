1b4079952e9014bdb119e52c7bd8fb60796c4e9e
Developer <mjasso@itexico.net>
2017-02-21 17:05:45 -0600

    playerUI - responsive image working

--- playerUI/src/app/body/search/search.component.ts
+++ playerUI/src/app/body/search/search.component.ts
@@ -8,27 +8,41 @@ import { HostListener } from '@angular/core';
   styleUrls: ['./search.component.scss'],
   providers: [SearchService],
 })
+
+
 export class SearchComponent implements OnInit {
   trackName: string;
   searchTerm: string;
   songsList: any;
   test: any;
+  imageSize: number;
+  smallScreen: boolean;
   constructor(private SearchService: SearchService) { }
 
   ngOnInit() {
-    this.test = screen.width;
+    console.log('debugging:');
+    console.log(window.innerWidth);
+    this.setScreen(window.innerWidth);
   }
 
+  @HostListener('window:resize', ['$event'])
   onResize(event) {
-     console.log(event.target.innerWidth); // window width
-   }
+    this.setScreen(event.target.innerWidth);
+  }
 
+  setScreen(screenWidth): any {
+      this.smallScreen = screenWidth < 768 ? true : false;
+      this.imageSize = screenWidth < 768 ? 3 : 1;
+  }
+
+  getImage(song): any {
+    return song.image[this.imageSize]['#text'];
+  }
   search(searchTerm): void {
     this.searchTerm = searchTerm;
     console.log('search clicked! - %s', searchTerm);
     this.SearchService.searchAPI(this.searchTerm).then((results) => {
       this.songsList = results;
-      console.log( results );
     });
   }// search
 }// searchComponent
