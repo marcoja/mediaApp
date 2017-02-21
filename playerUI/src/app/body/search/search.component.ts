0d3d401fbd8fcb192f9ae2b3a889d59ab1f85cfb
Marco Jasso <mjasso@itexico.net>
2017-02-20 23:56:46 -0600

    playerUI - song list working -semi responsive

--- playerUI/src/app/body/search/search.component.ts
+++ playerUI/src/app/body/search/search.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from './search.service';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
-  providers: [SearchService]
+  providers: [SearchService],
 })
 export class SearchComponent implements OnInit {
   trackName: string;
   searchTerm: string;
   songsList: any;
+  test: any;
   constructor(private SearchService: SearchService) { }
 
   ngOnInit() {
+    this.test = screen.width;
   }
+
+  onResize(event) {
+     console.log(event.target.innerWidth); // window width
+   }
 
   search(searchTerm): void {
     this.searchTerm = searchTerm;
